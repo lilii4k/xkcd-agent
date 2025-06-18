2977f2eeb8a0e370d0728fba7595c75dc1c269ca
Alexander Hein-Heifetz <aheifetz@rocketmail.com>
2025-06-18 04:56:55 -0400

    Agent starter (#7)
    * Integrate with Agent Boot Starter
    
    * Switch to Boot Parent POM
    
    * Removed extra space.
    
    * Upgrade to boot 3.5.0
    
    * Remove properties scan. Currently not required.

--- src/main/kotlin/com/embabel/template/ProjectNameApplication.kt
+++ src/main/kotlin/com/embabel/template/ProjectNameApplication.kt
@@ -15,35 +15,11 @@
  */
 package com.embabel.template
 
-import com.embabel.common.util.WinUtils
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    scanBasePackages = [
-        "com.embabel.agent",
-        "com.embabel.template",
-    ]
-)
-@ConfigurationPropertiesScan(
-    basePackages = [
-        "com.embabel.agent",
-        "com.embabel.template",
-    ]
-)
-class ProjectNameApplication {
-
-    companion object {
-        init {
-            if (WinUtils.IS_OS_WINDOWS()) {
-                // Set console to UTF-8 on Windows
-                // This is necessary to display non-ASCII characters correctly
-                WinUtils.CHCP_TO_UTF8()
-            }
-        }
-    }
-}
+@SpringBootApplication
+class ProjectNameApplication
 
 fun main(args: Array<String>) {
     runApplication<ProjectNameApplication>(*args)
